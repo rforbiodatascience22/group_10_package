567e4893448bec209706ac4ecfa981b6c40a02eb
Mikkel Wolfhagen <s184240@student.dtu.dk>
2022-03-21 10:10:24 +0000

    Updates divide codons

--- R/divide_codons.R
+++ R/divide_codons.R
@@ -1,6 +1,20 @@
+#' Divide into codons
+#'
+#' @param RNA_string
+#' @param start, reading frame
+#'
+#' @return Vector of all the codons included in the string
+#' @export
+#'
+#' @examples
+#'
+#'string <- "ATCGTACGATATGATACAGAGATAGACATATTTAACGG"
+#'divide_codons(string)
+#'"ATC" "GTA" "CGA" "TAT" "GAT" "ACA" "GAG" "ATA" "GAC" "ATA" "TTT" "AAC"
+
 divide_codons <- function(RNA_string, start = 1){
   RNA_len <- nchar(RNA_string)
-  codons <- substring(RNA_len,
+  codons <- substring(RNA_string,
                       first = seq(from = start, to = RNA_len-3+1, by = 3),
                       last = seq(from = 3+start-1, to = RNA_len, by = 3))
   return(codons)
