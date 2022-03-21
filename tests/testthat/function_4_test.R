56bf5edd9ea17901b62bf16b269dd823f93eb1aa
Zchafuz-Real <s220257@dtu.dk>
2022-03-21 14:37:34 +0000

    renamed other relevant to func 4

--- tests/testthat/function_4_test.R
+++
@@ -1,4 +0,0 @@
-test_that("my function works", {
-  expect_identical(function_4(codons=c("GAG","UGA","UGU")),"E*C")
-
-})
---
+++ tests/testthat/test-translate.R
@@ -0,0 +1,4 @@
+test_that("my function works", {
+  expect_identical(translate(codons=c("GAG","UGA","UGU")),"E*C")
+
+})
