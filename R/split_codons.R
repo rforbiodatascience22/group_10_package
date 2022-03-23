fa15f15af7afc235d588029a8e5b0b6e1dfae20b
9las <nilasschusler@gmail.com>
2022-03-23 15:40:18 +0000

    Fix typo in split_codons function (length->length)

--- R/split_codons.R
+++ R/split_codons.R
@@ -1,9 +1,9 @@
 #' Split RNA sequence into codons
 #'
 #' @param rna_string RNA sequence string
-#' @param start position in RNA sequence where open reading frame starts
+#' @param start position in RNA sequence where reading frame starts
 #'
-#' @return Vector of codons in open reading frame
+#' @return Vector of codons in reading frame
 #' @export
 #'
 #' @examples
@@ -12,9 +12,9 @@
 #' # "ATC" "GTA" "CGA" "TAT" "GAT" "ACA" "GAG" "ATA" "GAC" "ATA" "TTT" "AAC"
 
 split_codons <- function(rna_string, start = 1){
-  rna_lenght <- nchar(rna_string)
+  rna_length <- nchar(rna_string)
   codons <- substring(rna_string,
-                      first = seq(from = start, to = rna_lenght-3+1, by = 3),
-                      last = seq(from = 3+start-1, to = rna_lenght, by = 3))
+                      first = seq(from = start, to = rna_length-3+1, by = 3),
+                      last = seq(from = 3+start-1, to = rna_length, by = 3))
   return(codons)
 }
